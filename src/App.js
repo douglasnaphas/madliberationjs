71958a5733fa5006a5b4ef2f6fd39766f153e9c1
Douglas Naphas <douglasnaphas@gmail.com>
2018-10-09 20:17:44 -0400

    50 added react-router dom, routes

--- src/App.js
+++ src/App.js
@@ -3,20 +3,43 @@ import logo from "./logo.svg";
 import "./App.css";
 import ButtonRow from "./components/ButtonRow";
 import Header from "./components/Header";
+import { BrowserRouter as Router } from "react-router-dom";
+import Route from "react-router-dom/Route";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <ButtonRow
-          text="Log in"
-          primary
-          href="https://madliberationfederated.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=6ktt0mtpks03r8sfticc3h1o6&redirect_uri=https://madliberationgame.com/index.html"
-        />
-        <ButtonRow text="Start a seder" disabled />
-        <ButtonRow text="Join a seder" disabled />
-      </div>
+      <Router>
+        <div className="App">
+          <Route
+            path="/"
+            render={() => {
+              return (
+                <div>
+                  <Header />
+                  <ButtonRow
+                    text="Log in"
+                    primary
+                    href="https://madliberationfederated.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=6ktt0mtpks03r8sfticc3h1o6&redirect_uri=https://madliberationgame.com/index.html"
+                  />
+                  <ButtonRow text="Start a seder" disabled />
+                  <ButtonRow text="Join a seder" disabled />
+                </div>
+              );
+            }}
+          />
+          <Route
+            path="/r2"
+            render={() => {
+              return (
+                <div>
+                  <h1>This is R2</h1>
+                </div>
+              );
+            }}
+          />
+        </div>
+      </Router>
     );
 
     // buttonRow
