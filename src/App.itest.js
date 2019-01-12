0a4416a9a1914f401a5de2c517920329b7d56198
Douglas Naphas <douglasnaphas@gmail.com>
2019-01-11 20:50:18 -0500

    Parse opts with commander
    This parses options with commander, providing a nice help message, and
    adds a slow-mode option.
    
    madliberation-90

--- src/App.itest.js
+++ src/App.itest.js
@@ -1,24 +1,52 @@
 const puppeteer = require('puppeteer');
-if (process.argv.length < 3) {
-  process.exit(2);
+const program = require('commander');
+
+program
+  .version('1.0.0')
+  .option(
+    '-s, --site <URL>',
+    'Site to run against, default https://madliberationgame.com'
+  )
+  .option('-L, --slow', 'Run headfully in slow mode')
+  .parse(process.argv);
+const slowDown = 200;
+const timeoutMs = 80 + (program.slow ? slowDown + 2000 : 0);
+const defaultUrl = 'https://madliberationgame.com';
+const site = program.site || defaultUrl;
+const browserOptions = {
+  headless: program.slow ? false : true,
+  args: ['--no-sandbox']
+};
+if (program.slow) {
+  browserOptions.slowMo = slowDown;
 }
-const site = process.argv[2];
+
+const failTest = async (err, msg, browser) => {
+  console.log('test failed: ' + msg);
+  console.log(err);
+  await browser.close();
+  process.exit(1);
+};
+
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args:['--no-sandbox']
-  });
+  const browser = await puppeteer.launch(browserOptions);
   const page = await browser.newPage();
-  const waitForXPathOptions = { timeout: 40 };
+  const waitForXPathOptions = { timeout: timeoutMs };
   await page.goto(site);
 
+  // Home Page
   await page
     .waitForXPath('//*[text()="Start a seder"]', waitForXPathOptions)
     .catch(async e => {
-      console.log('button not found');
-      await browser.close();
-      process.exit(4);
+      failTest(e, 'Start a seder button not found', browser);
     });
+  await page
+    .waitForXPath('//*[text()="Join a seder"]', waitForXPathOptions)
+    .catch(async e => {
+      failTest(e, 'Join a seder button not found', browser);
+    });
+
+  // Start a Seder Page
 
   await browser.close();
 })();
