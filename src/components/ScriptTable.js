d821bfe387ef1772ca623be701e39b8c2601f684
Douglas Naphas <douglasnaphas@gmail.com>
2019-01-15 17:52:56 -0500

    Show scripts in a table
    madliberation-76

--- src/components/ScriptTable.js
+++ src/components/ScriptTable.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 // takes in JSON collection of scripts, and a function it will call to set
@@ -19,22 +25,25 @@ const styles = {
   }
 };
 
-const createFourThings = () => {
-  const things = [];
-  for (let i = 0; i < 4; i++) {
-    things[i] = <div>thing {i}</div>;
-  }
-  return things;
-};
-
 class ScriptTable extends React.Component {
   render() {
     const { scripts } = this.props;
     const scriptRows = [];
     for (let i = 0; i < scripts.length; i++) {
-      scriptRows[i] = <div>{scripts[i].haggadah_short_desc.S}</div>;
+      scriptRows[i] = (
+        <TableRow>
+          <TableCell>{scripts[i].haggadah_short_desc.S}</TableCell>
+          <TableCell>{scripts[i].haggadah_description.S}</TableCell>
+        </TableRow>
+      );
     }
-    return scriptRows;
+    return (
+      <div>
+        <Table>
+          <TableBody>{scriptRows}</TableBody>
+        </Table>
+      </div>
+    );
   }
 }
 
