db90e710b808ff1aa5920b1389747a6b5bfd476c
Douglas Naphas <douglasnaphas@gmail.com>
2019-01-31 21:00:41 -0500

    Generate a code on the Your Code page
    The Your Room Code Page generates a new random code and displays it. It
    includes the selected script's path in the post body of the fetch
    request.
    
    madliberation-91

--- src/components/YourRoomCodePage.js
+++ src/components/YourRoomCodePage.js
@@ -1,3 +1,5 @@
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Configs } from '../Configs';
 import React, { Component } from 'react';
 import MenuAppBar from './MenuAppBar';
 import { withStyles } from '@material-ui/core/styles';
@@ -12,18 +14,51 @@ const styles = theme => ({
 });
 
 class YourRoomCodePage extends Component {
+  componentDidMount() {
+    const roomCodeUrl = new URL('/room-code', Configs.apiUrl());
+    fetch(roomCodeUrl, {
+      method: 'POST',
+      body: JSON.stringify({
+        path: this.props.location.state.selectedScript.path.S
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(r => {
+        return r.json();
+      })
+      .then(j => {
+        this.setState({ roomCode: j.roomCode });
+        this.setState({ roomCodeLoading: false });
+      });
+  }
+
+  state = { roomCodeLoading: true, roomCode: '' };
   render() {
     const { classes, user } = this.props;
+    let spinnerOrRoomCode;
+    if (this.state.roomCodeLoading) {
+      spinnerOrRoomCode = (
+        <div>
+          <h2>Generating a Room Code...</h2>
+          <br />
+          <CircularProgress />
+        </div>
+      );
+    } else {
+      spinnerOrRoomCode = (
+        <div>
+          <h2>Your Room Code is:</h2>
+          <br />
+          <h2>{this.state.roomCode}</h2>
+        </div>
+      );
+    }
 
     return (
       <div madliberationid="your-room-code-page">
         <MenuAppBar user={user} />
-        <div>
-          <br />
-          <h2>This is your Room Code</h2>
-          <br />
-          <p>Tell the world.</p>
-        </div>
+        <br />
+        {spinnerOrRoomCode}
       </div>
     );
   }
