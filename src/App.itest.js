c10798870586aee4b88caf27e0f676524ca0b14b
Douglas Naphas <douglasnaphas@gmail.com>
2019-04-13 08:42:33 -0400

    Use IT helper functions
    madliberation-123

--- src/App.itest.js
+++ src/App.itest.js
@@ -17,37 +17,13 @@ const browserOptions = {
   headless: program.slow ? false : true,
   args: ['--no-sandbox']
 };
-// if (program.slow) {
 browserOptions.slowMo = slowDown;
-// }
 
 const failTest = async (err, msg, browser) => {
   console.log('test failed: ' + msg);
   console.log(err);
   if (browser) await browser.close();
   process.exit(1);
-};
-const waitForXPathOrFail = async ({
-  page,
-  xpath,
-  waitOptions,
-  elementDescription,
-  browser
-}) => {
-  await page.waitForXPath(xpath, waitOptions).catch(async e => {
-    failTest(e, 'Failed to find: ' + elementDescription, browser);
-  });
-};
-const waitForSelectorOrFail = async ({
-  page,
-  selector,
-  waitOptions,
-  elementDescription,
-  browser
-}) => {
-  await page.waitForSelector(selector, waitOptions).catch(async e => {
-    failTest(e, 'Failed to find: ' + elementDescription, browser);
-  });
 };
 const waitOptions = { timeout: timeoutMs /*, visible: true*/ };
 const waitForNavigationOptions = { timeout: timeoutMs };
@@ -123,167 +99,43 @@ const itGetText = async ({ page, madliberationid }) => {
     });
 
   // Pick Your Script Page
-  await Promise.all([
-    page.click('[madliberationid="lead-a-seder-button"]'),
-    page.waitForNavigation(waitForNavigationOptions)
-  ]).catch(async e => {
-    failTest(e, 'Failed to click Lead a seder button', browser);
-  });
-  ////////////////////////////////////////////////////////////////////////////////
-  await page
-    .waitForSelector('[madliberationid="pick-your-script-page"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'Pick Your Script Page not loaded', browser);
-    });
+  await itNavigate({ page: page, madliberationid: 'lead-a-seder-button' });
 
-  // About Page from menu button
-  // Wait for the menu icon
-  await page
-    .waitForSelector(
-      '[madliberationid="app-bar-menu-icon-button"]',
-      waitOptions
-    )
-    .catch(async e => {
-      failTest(e, 'Did not find menu button at top', browser);
-    });
-  // Click menu icon
-  await Promise.all([
-    page.click('[madliberationid="app-bar-menu-icon-button"]', clickOptions)
-  ]).catch(async e => {
-    failTest(e, 'Failed to click app bar menu icon', browser);
-  });
-  // Wait for menu item for About Page to be visible
-  await page
-    .waitForSelector('[madliberationid="menu-about-link"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'Did not find About link in menu', browser);
-    });
-  // Click menu item for About Page
-  await Promise.all([
-    page.click('[madliberationid="menu-about-link"]', clickOptions),
-    page.waitForNavigation(waitForNavigationOptions)
-  ]).catch(async e => {
-    failTest(e, 'Failed to click About link from menu', browser);
-  });
+  ////////////////////////////////////////////////////////////////////////////////
+
+  await itWait({ page: page, madliberationid: 'pick-your-script-page' });
+  await itClick({ page: page, madliberationid: 'app-bar-menu-icon-button' });
+  await itNavigate({ page: page, madliberationid: 'menu-about-link' });
+
   ////////////////////////////////////////////////////////////////////////////////
   // Confirm the About page is displayed
-  await page
-    .waitForSelector('[madliberationid="about-page"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'About Page not displayed', browser);
-    });
+  await itWait({ page: page, madliberationid: 'about-page' });
 
   // How to Play Page from menu button
-  // Wait for the menu icon
-  await page
-    .waitForSelector(
-      '[madliberationid="app-bar-menu-icon-button"]',
-      waitOptions
-    )
-    .catch(async e => {
-      failTest(e, 'Did not find menu button at top', browser);
-    });
-  // Click menu icon
-  await Promise.all([
-    page.click('[madliberationid="app-bar-menu-icon-button"]', clickOptions)
-  ]).catch(async e => {
-    failTest(e, 'Failed to click app bar menu icon', browser);
-  });
-  // Wait for menu item for How to Play Page to be visible
-  await page
-    .waitForSelector('[madliberationid="menu-how-to-play-link"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'Did not find How to Play link in menu', browser);
-    });
-  // Click menu item for How to Play Page
-  await Promise.all([
-    page.waitForNavigation(waitForNavigationOptions),
-    page.click('[madliberationid="menu-how-to-play-link"]', clickOptions)
-  ]).catch(async e => {
-    failTest(e, 'Failed to click How to Play link from menu', browser);
-  });
+  await itClick({ page: page, madliberationid: 'app-bar-menu-icon-button' });
+  await itNavigate({ page: page, madliberationid: 'menu-how-to-play-link' });
+
   ////////////////////////////////////////////////////////////////////////////////
   // Confirm the How to Play page is displayed
-  await page
-    .waitForSelector('[madliberationid="how-to-play-page"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'How to Play Page not displayed', browser);
-    });
+  await itWait({ page: page, madliberationid: 'how-to-play-page' });
 
-  // Back to Home Page with menu button
-  // Wait for the menu icon
-  await page
-    .waitForSelector(
-      '[madliberationid="app-bar-menu-icon-button"]',
-      waitOptions
-    )
-    .catch(async e => {
-      failTest(e, 'Did not find menu button at top', browser);
-    });
-  // Click menu icon
-  await Promise.all([
-    page.click('[madliberationid="app-bar-menu-icon-button"]', clickOptions)
-  ]).catch(async e => {
-    failTest(e, 'Failed to click app bar menu icon', browser);
-  });
-  // Wait for menu item for Home Page to be visible
-  await page
-    .waitForSelector('[madliberationid="menu-home-link"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'Did not find Home link in menu', browser);
-    });
-  // Click menu item for Home Page
-  await Promise.all([
-    page.click('[madliberationid="menu-home-link"]', clickOptions),
-    page.waitForNavigation(waitForNavigationOptions)
-  ]).catch(async e => {
-    failTest(e, 'Failed to click Home link from menu', browser);
-  });
+  // Go back to the Home Page
+  await itClick({ page: page, madliberationid: 'app-bar-menu-icon-button' });
+  await itNavigate({ page: page, madliberationid: 'menu-home-link' });
+
   ////////////////////////////////////////////////////////////////////////////////
 
   // Lead a seder
-  await page
-    .waitForSelector('[madliberationid="lead-a-seder-button"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'Lead a seder button not found', browser);
-    });
-  await Promise.all([
-    page.click('[madliberationid="lead-a-seder-button"]'),
-    page.waitForNavigation(waitForNavigationOptions)
-  ]).catch(async e => {
-    failTest(e, 'Failed to click Lead a seder button', browser);
-  });
+  await itNavigate({ page: page, madliberationid: 'lead-a-seder-button' });
+
   ////////////////////////////////////////////////////////////////////////////////
-  await page
-    .waitForSelector('[madliberationid="pick-your-script-page"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'Pick Your Script Page not loaded', browser);
-    });
+
+  await itWait({ page: page, madliberationid: 'pick-your-script-page' });
+
   // Pick the Practice Script
-  const practiceScriptSelector = '[madliberationid="Practice Script"]';
-  await page
-    .waitForSelector(practiceScriptSelector, waitOptions)
-    .catch(async e => {
-      failTest(e, 'Could not find Practice Script in scripts table', browser);
-    });
-  await Promise.all([page.click(practiceScriptSelector)]).catch(async e => {
-    failTest(e, 'Failed to select Practice Script', browser);
-  });
-  await page
-    .waitForSelector('[madliberationid="pick-this-script-button"]', waitOptions)
-    .catch(async e => {
-      failTest(
-        e,
-        'Could not find Use This One button after picking script',
-        browser
-      );
-    });
-  await Promise.all([
-    page.click('[madliberationid="pick-this-script-button"]'),
-    page.waitForNavigation(waitForNavigationOptions)
-  ]).catch(async e => {
-    failTest(e, 'Failed to click Use This One button, scripts table', browser);
-  });
+  await itClick({ page: page, madliberationid: 'Practice Script' });
+  await itNavigate({ page: page, madliberationid: 'pick-this-script-button' });
+
   ////////////////////////////////////////////////////////////////////////////////
   // Wait for a Room Code to appear
   await itWait({ page: page, madliberationid: 'your-room-code' });
@@ -314,172 +166,69 @@ const itGetText = async ({ page, madliberationid }) => {
 
   ////////////////////////////////////////////////////////////////////////////////
 
-  await page2
-    .waitForSelector('[madliberationid="enter-room-code-page"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'Enter Room Code Page not displayed', browser2);
-    });
+  await itWait({ page: page2, madliberationid: 'enter-room-code-page' });
+
   // Enter Room Code and Game Name
-  await page2
-    .waitForSelector(
-      '[madliberationid="enter-room-code-text-field"]',
-      waitOptions
-    )
-    .catch(async e => {
-      failTest(e, 'Could not find Enter Room Code text field');
-    });
-  await page2
-    .waitForSelector('[madliberationid="game-name-text-field"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'Could not find Game Name text field');
-    });
-  await page2
-    .waitForSelector('[madliberationid="join-this-seder-button"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'Could not find Join button');
-    });
   const player2Name = `ITestP2 ${roomCode}`;
-  await page2
-    .click('[madliberationid="enter-room-code-text-field"]', clickOptions)
-    .catch(async e => {
-      failTest(e, 'Could not click Enter Room Code text field');
-    });
-  await page2.type(
-    '[madliberationid="enter-room-code-text-field"]',
-    roomCode,
-    typeOptions
-  );
-  await page2.click('[madliberationid="game-name-text-field"]', clickOptions);
-  await page2.type(
-    '[madliberationid="game-name-text-field"]',
-    player2Name,
-    typeOptions
-  );
-  await Promise.all([
-    page2.click('[madliberationid="join-this-seder-button"]', clickOptions),
-    page2.waitForNavigation(waitForNavigationOptions)
-  ]).catch(async e => {
-    failTest(e, 'Failed to click Join a seder button', browser2);
+  await itType({
+    page: page2,
+    madliberationid: 'enter-room-code-text-field',
+    text: roomCode
   });
+  await itType({
+    page: page2,
+    madliberationid: 'game-name-text-field',
+    text: player2Name
+  });
+  await itNavigate({ page: page2, madliberationid: 'join-this-seder-button' });
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
   // Leader checks roster
   // Click No, Check Again so Player 2 shows up on the roster
-  await page
-    .waitForSelector('[madliberationid="no-check-again-button"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'Could not find No Check Again button on roster');
-    });
-  await Promise.all([
-    page.click('[madliberationid="no-check-again-button"]', clickOptions)
-  ]).catch(async e => {
-    failTest(e, 'Failed to click No Check Again button');
-  });
+  await itClick({ page: page, madliberationid: 'no-check-again-button' });
   // Verify both players are on the roster
-  await page
-    .waitForSelector('[madliberationid="pc0"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'Could not find first row in roster');
-    });
-  await page
-    .waitForSelector('[madliberationid="pc1"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'Could not find second row in roster');
-    });
-  const leaderNameFromTable = await page
-    .$$('[madliberationid="pc0"]')
-    .then(a => {
-      if (!Array.isArray(a) || a.length < 1) {
-        throw new Error('Could not get array with Participant Cell 0');
-      }
-      return a[0].getProperty('textContent');
-    })
-    .then(textContent => {
-      return textContent.jsonValue();
-    })
-    .catch(async e => {
-      failTest(e, 'Failed to get text from roster Participant Cell 0');
-    });
+  const leaderNameFromTable = await itGetText({
+    page: page,
+    madliberationid: 'pc0'
+  });
   if (leaderNameFromTable != leaderName) {
     failTest(
       'wrong name on roster',
       `expected ${leaderName}, got ` + `${leaderNameFromTable}`
     );
   }
-  const p2NameFromTable = await page
-    .$$('[madliberationid="pc1"]')
-    .then(a => {
-      if (!Array.isArray(a) || a.length < 1) {
-        throw new Error('Could not get array with Participant Cell 1');
-      }
-      return a[0].getProperty('textContent');
-    })
-    .then(textContent => {
-      return textContent.jsonValue();
-    })
-    .catch(async e => {
-      failTest(e, 'Failed to get text from roster Participant Cell 1');
-    });
+  const p2NameFromTable = await itGetText({
+    page: page,
+    madliberationid: 'pc1'
+  });
   if (p2NameFromTable != player2Name) {
     failTest(
       'wrong name on roster',
       `expected ${player2Name}, got ` + `${p2NameFromTable}`
     );
   }
+
   // Click Thats Everyone
-  await page
-    .waitForSelector('[madliberationid="thats-everyone-button"]', waitOptions)
-    .catch(async e => {
-      failTest(e, "Could not find That's Everyone button on roster");
-    });
-  await Promise.all([
-    page.click('[madliberationid="thats-everyone-button"]', clickOptions),
-    page.waitForNavigation(waitForNavigationOptions)
-  ]).catch(async e => {
-    failTest(e, 'Failed to click Thats Everyone button', browser);
-  });
+  await itNavigate({ page: page, madliberationid: 'thats-everyone-button' });
+
   ////////////////////////////////////////////////////////////////////////////////
 
   // Leader: get assignments, complete them, and submit
-  await page
-    .waitForSelector(
-      '[madliberationid="leader-click-this-button"]',
-      waitOptions
-    )
-    .catch(async e => {
-      failTest(e, "Could not find leader's Click This Button");
-    });
-  await Promise.all([
-    page.click('[madliberationid="leader-click-this-button"]', clickOptions),
-    page.waitForNavigation(waitForNavigationOptions)
-  ]).catch(async e => {
-    failTest(e, "Failed to click Leader's Button");
-  });
+  await itNavigate({ page: page, madliberationid: 'leader-click-this-button' });
+
   ////////////////////////////////////////////////////////////////////////////////
 
   // Make sure we're on the /play page
-  await page
-    .waitForSelector('[madliberationid="lib-progress"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'lib progres did not appear');
-    });
+  await itWait({ page: page, madliberationid: 'lib-progress' });
 
   const submitAllLibs = async (page, prefix) => {
     const answers = [];
-    const progressText = await page
-      .$$('[madliberationid="lib-progress"]')
-      .then(a => {
-        if (!Array.isArray(a) || a.length < 1) {
-          throw new Error(`Could not grab progress text for ${prefix}`);
-        }
-        return a[0].getProperty('textContent');
-      })
-      .then(textContent => textContent.jsonValue())
-      .catch(async e => {
-        failTest(e, `Failed to get text from lib progress for ${prefix}`);
-      });
+    const progressText = await itGetText({
+      page: page,
+      madliberationid: 'lib-progress'
+    });
     const progress = progressText.split('/').map(n => parseInt(n.trim()));
     if (progress.length < 2) {
       failTest('/play page', 'did not find X / Y showing lib progress');
@@ -488,67 +237,23 @@ const itGetText = async ({ page, madliberationid }) => {
     for (let currentLib = progress[0]; currentLib <= numLibs; currentLib++) {
       // Enter a lib, save it to answers
       const ans = `${prefix}-${currentLib}`;
-      await page
-        .waitForSelector(
-          `[madliberationid="answer-${currentLib - 1}"]`,
-          waitOptions
-        )
-        .catch(async e => {
-          failTest(e, `Could not find text box for answer ${currentLib}`);
-        });
-      await page
-        .click(`[madliberationid="answer-${currentLib - 1}"]`, waitOptions)
-        .catch(async e => {
-          failTest(e, `Failed to click answer box ${currentLib}`);
-        });
-      await page
-        .type(`[madliberationid="answer-${currentLib - 1}"]`, ans, typeOptions)
-        .catch(async e => {
-          failTest(e, `Failed to enter answer ${ans}`);
-        });
+      await itType({
+        page: page,
+        madliberationid: `answer-${currentLib - 1}`,
+        text: ans
+      });
       answers.push(ans);
       // If we're on the last lib, submit and return
       if (currentLib === numLibs) {
-        await page
-          .waitForSelector('[madliberationid="submit-answers"]')
-          .catch(async e => {
-            failTest(
-              e,
-              `could not find Submit libs button, prompt ${currentLib}`
-            );
-          });
-        await page
-          .click('[madliberationid="submit-answers"]')
-          .catch(async e => {
-            failTest(e, `failed to click Submit libs, prompt ${currentLib}`);
-          });
-        await page
-          .waitForSelector('[madliberationid="yes-submit-libs-button"]')
-          .catch(async e => {
-            failTest(
-              e,
-              `could not find confirm submit button, prompt ${currentLib}`
-            );
-          });
-        await Promise.all([
-          page.click('[madliberationid="yes-submit-libs-button"]'),
-          page.waitForNavigation(waitForNavigationOptions)
-        ]).catch(async e => {
-          failTest(e, `failed to confirm submit, prompt ${currentLib}`);
+        await itClick({ page: page, madliberationid: 'submit-answers' });
+        await itNavigate({
+          page: page,
+          madliberationid: 'yes-submit-libs-button'
         });
         return answers;
       }
       // Click the Next button
-      await page
-        .waitForSelector('[madliberationid="next-prompt"]', waitOptions)
-        .catch(async e => {
-          failTest(e, 'no next prompt button');
-        });
-      await page
-        .click('[madliberationid="next-prompt"]', clickOptions)
-        .catch(async e => {
-          failTest(e, 'could not click next prompt');
-        });
+      await itClick({ page: page, madliberationid: 'next-prompt' });
     }
     failTest('/play page', 'never found a Submit Answers button');
   };
@@ -558,28 +263,15 @@ const itGetText = async ({ page, madliberationid }) => {
   ////////////////////////////////////////////////////////////////////////////////
 
   // Player 2: get assignments, complete them, and submit
-  await page2
-    .waitForSelector(
-      '[madliberationid="player-click-this-button"]',
-      waitOptions
-    )
-    .catch(async e => {
-      failTest(e, "Could not find player's Click This Button");
-    });
-  await Promise.all([
-    page2.click('[madliberationid="player-click-this-button"]', clickOptions),
-    page2.waitForNavigation(waitForNavigationOptions)
-  ]).catch(async e => {
-    failTest(e, "Failed to click Player's Button");
+  await itNavigate({
+    page: page2,
+    madliberationid: 'player-click-this-button'
   });
+
   ////////////////////////////////////////////////////////////////////////////////
 
   // Make sure we're on the /play page
-  await page2
-    .waitForSelector('[madliberationid="lib-progress"]', waitOptions)
-    .catch(async e => {
-      failTest(e, 'lib progres did not appear');
-    });
+  await itWait({ page: page2, madliberationid: 'lib-progress' });
   const p2Answers = await submitAllLibs(page2, 'p2');
 
   // Confirm player submissions appeared in the script
