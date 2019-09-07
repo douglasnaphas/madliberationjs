eb5e6e740d05e13adefbfa9f6df74adf25e03a66
Douglas Naphas <douglasnaphas@gmail.com>
2019-09-07 15:33:53 -0400

    Remove unused Home Pages
    https://github.com/douglasnaphas/madliberation/issues/110

--- src/App.test.js
+++ src/App.test.js
@@ -4,15 +4,9 @@ import { createMount } from '@material-ui/core/test-utils';
 import React from 'react';
 
 import App from './App';
-import LoggedInHomePage from './components/LoggedInHomePage';
 import NoAuthHomePage from './components/NoAuthHomePage';
-import PublicHomePage from './components/PublicHomePage';
-import { signIn } from './lib/cognito';
 
 configure({ adapter: new Adapter() });
-
-jest.mock('./lib/cognito');
-// signIn.mockReturnValue(Promise.reject(null));
 
 describe('<App />', () => {
   let mount;
@@ -25,39 +19,9 @@ describe('<App />', () => {
     mount.cleanUp();
   });
 
-  test.skip('when no user is logged in, there should be a log in button', () => {
-    const wrapper = mount(<App />);
-    expect(wrapper.containsMatchingElement(<PublicHomePage />)).toBeTruthy();
-    expect(wrapper.containsMatchingElement(<LoggedInHomePage />)).toBeFalsy();
-  });
-
-  test.skip('when the user sign in succeeds it shows the logged-in home page', async () => {
-    const user = {
-      id: '123',
-      email: 'sederer@example.com',
-      nickname: 'the dude'
-    };
-    signIn.mockReturnValue(Promise.resolve(user));
-    const wrapper = await mount(<App />);
-    wrapper.update();
-    expect(wrapper.containsMatchingElement(<LoggedInHomePage />)).toBeTruthy();
-    expect(wrapper.containsMatchingElement(<PublicHomePage />)).toBeFalsy();
-  });
-
-  test('a test', () => {});
-
   test('/ should show the NoAuthHomePage', async () => {
     const wrapper = await mount(<App />);
     wrapper.update();
     expect(wrapper.containsMatchingElement(<NoAuthHomePage />)).toBeTruthy();
   });
-
-  // integration tests
-  // test('Join a seder flow', async () => {
-  //   const wrapper = await mount(<App />);
-  //   wrapper.update();
-  //   wrapper.find('Button[title="Join a seder"]').simulate('click');
-  //   wrapper.update();
-  //   console.log(wrapper.debug());
-  // });
 });
