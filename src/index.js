83b6d9535c046ae425d21d4fb2d8269c41fd6ee2
Douglas Naphas <douglasnaphas@gmail.com>
2019-09-08 16:32:04 -0400

    Remove old deps
    https://github.com/douglasnaphas/madliberation/issues/191
    
    Some peer deps are still needed.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
