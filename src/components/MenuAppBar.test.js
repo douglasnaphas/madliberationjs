727d0011068daf6ef12b8af480605c16c7cdade9
Douglas Naphas <douglasnaphas@gmail.com>
2019-10-13 01:05:40 -0400

    Test MenuAppBar

--- src/components/MenuAppBar.test.js
+++ src/components/MenuAppBar.test.js
@@ -1,10 +1,12 @@
 import { createMount } from '@material-ui/core/test-utils';
+import { Link } from 'react-router-dom';
 import { MemoryRouter } from 'react-router-dom';
 import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
 
 import MenuAppBar from './MenuAppBar';
 
-describe('testing <App />', () => {
+describe('MenuAppBar', () => {
   let mount;
 
   beforeEach(() => {
@@ -15,12 +17,53 @@ describe('testing <App />', () => {
     mount.cleanUp();
   });
 
-  test.skip('Menu should link to Home, About, and How to Play', async () => {
-    const wrapper = await mount(
+  test('Menu should link to Home, About, and How to Play', () => {
+    const wrapper = mount(
       <MemoryRouter>
         <MenuAppBar />
       </MemoryRouter>
     );
+    let homeLink = wrapper.findWhere(
+      n => n.is(Link) && n.is({ madliberationid: 'menu-home-link' })
+    );
+    expect(homeLink.exists()).toBe(false);
+    let aboutLink = wrapper.findWhere(
+      n => n.is(Link) && n.is({ madliberationid: 'menu-about-link' })
+    );
+    expect(aboutLink.exists()).toBe(false);
+    let howToPlayLink = wrapper.findWhere(
+      n => n.is(Link) && n.is({ madliberationid: 'menu-how-to-play-link' })
+    );
+    expect(howToPlayLink.exists()).toBe(false);
+    let menuButton = wrapper.findWhere(
+      n =>
+        n.is(IconButton) &&
+        n.is({
+          madliberationid: 'app-bar-menu-icon-button'
+        })
+    );
+    const target = window.document.querySelector(
+      `[madliberationid="app-bar-menu-icon-button"]`
+    );
+    const mockEventForClick = {
+      currentTarget: target
+    };
+    menuButton.props().onClick(mockEventForClick);
     wrapper.update();
+    homeLink = wrapper.findWhere(
+      n => n.is(Link) && n.is({ madliberationid: 'menu-home-link' })
+    );
+    expect(homeLink.exists()).toBe(true);
+
+    aboutLink = wrapper.findWhere(
+      n => n.is(Link) && n.is({ madliberationid: 'menu-about-link' })
+    );
+    expect(aboutLink.exists()).toBe(true);
+    howToPlayLink = wrapper.findWhere(
+      n => n.is(Link) && n.is({ madliberationid: 'menu-how-to-play-link' })
+    );
+    expect(howToPlayLink.exists()).toBe(true);
+    const menu = wrapper.find('#leftMenu').first();
+    expect(menu.prop('anchorEl')).toBe(target);
   });
 });
