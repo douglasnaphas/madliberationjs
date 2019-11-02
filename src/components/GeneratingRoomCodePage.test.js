129143d2d1bb4fb3fb0075e83762a266dd36e198
Douglas Naphas <douglasnaphas@gmail.com>
2019-11-02 17:33:07 -0400

    Show how to mock localStorage
    Setting global.localStorage, as recommended by many sources, was not
    working.

--- src/components/GeneratingRoomCodePage.test.js
+++ src/components/GeneratingRoomCodePage.test.js
@@ -2,10 +2,13 @@ import { createMount } from '@material-ui/core/test-utils';
 import { MemoryRouter } from 'react-router-dom';
 import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import GeneratingRoomCodePage from './GeneratingRoomCodePage';
 import { Typography } from '@material-ui/core';
 
+import { Configs } from '../Configs';
+import GeneratingRoomCodePage from './GeneratingRoomCodePage';
+
 let mount;
+
 beforeEach(() => {
   mount = createMount();
 });
@@ -52,7 +55,210 @@ describe('GeneratingRoomCodePageWithRouter', () => {
       done();
     });
   });
-  test('should fetch /room-code', () => {});
-  test('chosenPath should be hydrated if not supplied', () => {});
+  test('should fetch /room-code', done => {
+    const mockSuccessResponse = {};
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise
+    });
+    jest.spyOn(global, 'fetch').mockImplementation(() => {
+      return mockFetchPromise;
+    });
+    const history = {
+      push: jest.fn()
+    };
+    const setChosenPath = jest.fn();
+    const setConfirmedRoomCode = jest.fn();
+    const chosenPath = 'a/b/c';
+    const wrapper = mount(
+      <MemoryRouter>
+        <GeneratingRoomCodePage
+          history={history}
+          setChosenPath={setChosenPath}
+          chosenPath={chosenPath}
+          setConfirmedRoomCode={setConfirmedRoomCode}
+        ></GeneratingRoomCodePage>
+      </MemoryRouter>
+    );
+    expect(global.fetch).toHaveBeenCalled();
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(
+      new URL('/room-code', Configs.apiUrl()),
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          path: chosenPath
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      }
+    );
+    process.nextTick(() => {
+      global.fetch.mockClear();
+      done();
+    });
+  });
+  test('should set confirmedRoomCode on successful fetch', done => {
+    const mockSuccessResponse = { roomCode: 'SUCCES' };
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise
+    });
+    jest.spyOn(global, 'fetch').mockImplementation(() => {
+      return mockFetchPromise;
+    });
+    const history = {
+      push: jest.fn()
+    };
+    const setChosenPath = jest.fn();
+    const setConfirmedRoomCode = jest.fn();
+    const chosenPath = 'a/b/c';
+    const wrapper = mount(
+      <MemoryRouter>
+        <GeneratingRoomCodePage
+          history={history}
+          setChosenPath={setChosenPath}
+          chosenPath={chosenPath}
+          setConfirmedRoomCode={setConfirmedRoomCode}
+        ></GeneratingRoomCodePage>
+      </MemoryRouter>
+    );
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    process.nextTick(() => {
+      expect(setConfirmedRoomCode).toHaveBeenCalled();
+      expect(setConfirmedRoomCode).toHaveBeenCalledTimes(1);
+      expect(setConfirmedRoomCode).toHaveBeenCalledWith('SUCCES');
+      global.fetch.mockClear();
+      done();
+    });
+  });
+  test('should set confirmedRoomCode on successful fetch 2', done => {
+    const mockSuccessResponse = { roomCode: 'SECOND' };
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise
+    });
+    jest.spyOn(global, 'fetch').mockImplementation(() => {
+      return mockFetchPromise;
+    });
+    const history = {
+      push: jest.fn()
+    };
+    const setChosenPath = jest.fn();
+    const setConfirmedRoomCode = jest.fn();
+    const chosenPath = 'a/b/c';
+    const wrapper = mount(
+      <MemoryRouter>
+        <GeneratingRoomCodePage
+          history={history}
+          setChosenPath={setChosenPath}
+          chosenPath={chosenPath}
+          setConfirmedRoomCode={setConfirmedRoomCode}
+        ></GeneratingRoomCodePage>
+      </MemoryRouter>
+    );
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    process.nextTick(() => {
+      expect(setConfirmedRoomCode).toHaveBeenCalled();
+      expect(setConfirmedRoomCode).toHaveBeenCalledTimes(1);
+      expect(setConfirmedRoomCode).toHaveBeenCalledWith('SECOND');
+      global.fetch.mockClear();
+      done();
+    });
+  });
+  test('should push onto history', done => {
+    const mockSuccessResponse = { roomCode: 'HISTOR' };
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise
+    });
+    jest.spyOn(global, 'fetch').mockImplementation(() => {
+      return mockFetchPromise;
+    });
+    const history = {
+      push: jest.fn()
+    };
+    const setChosenPath = jest.fn();
+    const setConfirmedRoomCode = jest.fn();
+    const chosenPath = 'a/b/c';
+    const wrapper = mount(
+      <MemoryRouter>
+        <GeneratingRoomCodePage
+          history={history}
+          setChosenPath={setChosenPath}
+          chosenPath={chosenPath}
+          setConfirmedRoomCode={setConfirmedRoomCode}
+        ></GeneratingRoomCodePage>
+      </MemoryRouter>
+    );
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    process.nextTick(() => {
+      expect(history.push).toHaveBeenCalled();
+      expect(history.push).toHaveBeenCalledTimes(1);
+      expect(history.push).toHaveBeenCalledWith('/your-room-code');
+      global.fetch.mockClear();
+      done();
+    });
+  });
+  test('chosenPath should be hydrated if not supplied, but present in storage', done => {
+    const mockSuccessResponse = { roomCode: 'LOCALS' };
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise
+    });
+    jest.spyOn(global, 'fetch').mockImplementation(() => {
+      return mockFetchPromise;
+    });
+    const history = {
+      push: jest.fn()
+    };
+    const setChosenPath = jest.fn();
+    const setConfirmedRoomCode = jest.fn();
+    const chosenPath = undefined;
+    // global.localStorage.getItem = jest.fn(() => 'un/supp/lied');
+
+    // global.localStorage.getItem = () => {
+    //   console.log('localStorage called!!!!');
+    //   return 'gotten';
+    // };
+    const spy = jest
+      .spyOn(Storage.prototype, 'getItem')
+      .mockImplementation(() => {
+        // console.log('getItem .... calllllled');
+        return 'gottttt';
+      });
+
+    const wrapper = mount(
+      <MemoryRouter>
+        <GeneratingRoomCodePage
+          history={history}
+          setChosenPath={setChosenPath}
+          chosenPath={chosenPath}
+          setConfirmedRoomCode={setConfirmedRoomCode}
+        ></GeneratingRoomCodePage>
+      </MemoryRouter>
+    );
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    console.log(global.localStorage.getItem('hi'));
+
+    // expect(global.fetch).toHaveBeenCalledWith(
+    //   new URL('/room-code', Configs.apiUrl()),
+    //   {
+    //     method: 'POST',
+    //     body: JSON.stringify({
+    //       path: 'gotten'
+    //     }),
+    //     headers: { 'Content-Type': 'application/json' }
+    //   }
+    // );
+
+    process.nextTick(() => {
+      expect(history.push).toHaveBeenCalled();
+      expect(history.push).toHaveBeenCalledTimes(1);
+      expect(history.push).toHaveBeenCalledWith('/your-room-code');
+      // expect(global.localStorage.getItem).toHaveBeenCalled();
+
+      global.fetch.mockClear();
+      done();
+    });
+  });
 });
-// https://medium.com/@rishabhsrao/mocking-and-testing-fetch-with-jest-c4d670e2e167
