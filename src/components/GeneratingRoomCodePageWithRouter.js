51660d41dccbb28ee53e96f7b92f6d91e9d79777
Douglas Naphas <douglasnaphas@gmail.com>
2020-01-07 21:05:48 -0500

    Remove unused imports

--- src/components/GeneratingRoomCodePageWithRouter.js
+++ src/components/GeneratingRoomCodePageWithRouter.js
@@ -1,10 +1,4 @@
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { Configs } from '../Configs';
-import MenuAppBar from './MenuAppBar';
-import React, { Component } from 'react';
-import { Typography } from '@material-ui/core';
 import { withRouter } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles';
 import GeneratingRoomCodePage from './GeneratingRoomCodePage';
 
 export default withRouter(GeneratingRoomCodePage);
