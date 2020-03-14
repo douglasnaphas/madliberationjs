4cd715ece38ceb20696a8a8fefbd67b3cf50c738
Douglas Naphas <douglasnaphas@gmail.com>
2020-03-14 16:28:31 -0400

    Remove itest run to make debugging go faster
    https://github.com/douglasnaphas/madliberation/issues/200

--- itest.sh
+++ itest.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-IT_ROOM_CODE=$(npx madliberation-itest --site https://staging.madliberationgame.com | awk '{print $3}')
-[[ -n ${IT_ROOM_CODE} ]]
-echo "created room code ${IT_ROOM_CODE}"
+# IT_ROOM_CODE=$(npx madliberation-itest --site https://staging.madliberationgame.com | awk '{print $3}')
+# [[ -n ${IT_ROOM_CODE} ]]
+# echo "created room code ${IT_ROOM_CODE}"
 
 # cleanup
 aws sts get-caller-identity
