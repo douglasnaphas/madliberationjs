b941ede97c075505835cfbaf174bcb0196d8ff1f
Douglas Naphas <douglasnaphas@gmail.com>
2020-03-14 15:51:15 -0400

    Install jq
    https://github.com/douglasnaphas/madliberation/issues/200

--- itest.sh
+++ itest.sh
@@ -3,11 +3,28 @@
 IT_ROOM_CODE=$(npx madliberation-itest --site https://staging.madliberationgame.com | awk '{print $3}')
 [[ -n ${IT_ROOM_CODE} ]]
 echo "created room code ${IT_ROOM_CODE}"
+
+# cleanup
 aws sts get-caller-identity
+
+# there should be 3 items
 aws dynamodb query \
   --table-name seders \
   --key-condition-expression "room_code = :rc" \
   --expression-attribute-values "{\":rc\":{\"S\":\"${IT_ROOM_CODE}\"}}" \
   | \
   jq '.["Items"] | length'
-  
+# that should print 3
+
+# one should be a seder, the other two should start with participant#
+
+
+# there should be a participant in this seder with the name "ITestLdr <room code>"
+
+# there should be a participant in this seder with the name "ITestP2 <room code>"
+
+# 
+
+# seder
+# lib_id should be seder
+# closed should be true
