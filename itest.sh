124e28bbba3e3742caf43d6fbd851b5fcd406d67
Douglas Naphas <douglasnaphas@gmail.com>
2020-03-14 14:19:41 -0400

    Query dynamo from the build
    https://github.com/douglasnaphas/madliberation/issues/200

--- itest.sh
+++ itest.sh
@@ -3,3 +3,11 @@
 IT_ROOM_CODE=$(npx madliberation-itest --site https://staging.madliberationgame.com | awk '{print $3}')
 [[ -n ${IT_ROOM_CODE} ]]
 echo "created room code ${IT_ROOM_CODE}"
+aws sts get-caller-identity
+aws dynamodb query \
+  --table-name seders \
+  --key-condition-expression "room_code = :rc" \
+  --expression-attribute-values "{\":rc\":{\"S\":\"${IT_ROOM_CODE}\"}}" \
+  | \
+  jq '.["Items"] | length'
+  
