c485c39fb7100197767e9d14f42b3690aed08ef2
Douglas Naphas <douglasnaphas@gmail.com>
2021-01-26 17:58:32 -0800

    Start a Configs test, note a TODO in Configs

--- src/Configs.js
+++ src/Configs.js
@@ -7,7 +7,7 @@ class Configs {
       }
       // if test env, return test env value
     }
-    return 'https://api.passover.lol/get-cookies';
+    return 'https://api.passover.lol/get-cookies'; // maybe use the current origin
   }
 
   // https://madliberationfederated.auth.us-east-1.amazoncognito.com/login?client_id=25h54vd0cundt7iaeon1rn8a02&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=http://localhost:3000/index.html
---
+++ src/Configs.test.js
@@ -0,0 +1,7 @@
+import { Configs } from './Configs';
+
+describe('Configs', () => {
+  describe('apiFetchTarget', () => {
+    test('apiFetchTarget', () => {});
+  });
+});
