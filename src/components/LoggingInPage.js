ebd3b9d4461cf9f56bf405c87bcaf5b9c65145ae
Douglas Naphas <douglasnaphas@gmail.com>
2021-02-21 11:07:36 -0700

    Apply Prettier formatting to LoggingInPage.js

--- src/components/LoggingInPage.js
+++ src/components/LoggingInPage.js
@@ -1,41 +1,41 @@
-import React, { useEffect } from 'react';
-import MenuAppBar from './MenuAppBar';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import { Configs } from '../Configs';
+import React, { useEffect } from "react";
+import MenuAppBar from "./MenuAppBar";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import { Configs } from "../Configs";
 
-const styles = theme => ({
+const styles = (theme) => ({
   button: {
-    margin: theme.spacing(1)
+    margin: theme.spacing(1),
   },
   input: {
-    display: 'none'
-  }
+    display: "none",
+  },
 });
 
 function LoggingInPage({ history, setUser, browserWindow, storage }) {
   useEffect(() => {
-    const idUrl = new URL('/id', Configs.apiUrl());
+    const idUrl = new URL("/id", Configs.apiUrl());
     fetch(idUrl, {
-      method: 'GET',
-      credentials: 'include'
+      method: "GET",
+      credentials: "include",
     })
-      .then(r => {
+      .then((r) => {
         return r.json();
       })
-      .then(user => {
+      .then((user) => {
         setUser(user);
-        storage.setItem('user-nickname', user.nickname);
-        storage.setItem('user-email', user.email);
-        storage.setItem('user-sub', user.sub);
+        storage.setItem("user-nickname", user.nickname);
+        storage.setItem("user-email", user.email);
+        storage.setItem("user-sub", user.sub);
         const uri = browserWindow.location.toString();
-        if (uri.indexOf('?') > 0) {
-          const clean_uri = uri.substring(0, uri.indexOf('?'));
+        if (uri.indexOf("?") > 0) {
+          const clean_uri = uri.substring(0, uri.indexOf("?"));
           browserWindow.history.replaceState({}, document.title, clean_uri);
         }
-        history.push('/');
+        history.push("/");
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, [history, setUser, browserWindow, storage]);
